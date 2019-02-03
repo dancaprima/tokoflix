97c1b834714e0c8cd9fe92e08f879ad8964e01fd
dancaprima <dancaprimaa@gmail.com>
2019-02-03 12:23:26 +0700

    Try to dispatch action in container

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,12 +1,46 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchMovies } from '../../services/movies/action';
 
 class Home extends React.Component {
   constructor(props) {
     super(props);
   }
+
+  componentDidMount() {
+    this.props.dispatch(fetchMovies());
+  }
+
+  renderCard(data) {
+    return (
+      <div>
+        <img src={data.poster_path} alt="" />
+        <h1>{data.original_title}</h1>
+        <h3>{data.vote_average}</h3>
+        <p />
+      </div>
+    );
+  }
+
   render() {
-    return <div>Home</div>;
+    const { movies } = this.props;
+    return (
+      <div>
+        {movies && movies.data &&  movies.data.map(d => this.renderCard(d))}
+      </div>
+    );
   }
 }
+const mapStateToProps = state => {
+  const { movies } = state;
+  return { movies };
+};
 
-export default Home;
+const mapDispatchToProps = dispatch => {
+  return { dispatch };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Home);
