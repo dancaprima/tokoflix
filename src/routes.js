69745c3791947d23968c21fddba88cdc2b1cae76
dancaprima <dancaprimaa@gmail.com>
2019-02-03 12:23:52 +0700

    Revise routes path name

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import Home from './containers/Home/Home.js';
-import Detail from './containers/Detail/Detail.js';
+import Home from './containers/Home/Home';
+import Detail from './containers/Detail/Detail';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 const Routes = ({ store }) => (
