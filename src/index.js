9f489b6fbf98495f1a3ea012472598f1a8318598
dancaprima <dancaprimaa@gmail.com>
2019-02-03 12:20:55 +0700

    Apply thunk middleware

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import Routes from './routes';
 import * as serviceWorker from './serviceWorker';
+import rootReducer from './services/rootReducer';
 
-function todoApp(state = {}, action) {
-  return state;
-}
-const store = createStore(todoApp);
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 ReactDOM.render(<Routes store={store} />, document.getElementById('root'));
 
