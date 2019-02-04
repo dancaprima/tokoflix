b991c1e640c4c47b47ef89e39c49bc81022d1039
dancaprima <dancaprimaa@gmail.com>
2019-02-04 11:34:30 +0700

    Add convert from path to id function

--- src/helpers/pathPrettier.js
+++ src/helpers/pathPrettier.js
@@ -1,6 +1,11 @@
 export const convertToPath = (movieId, movieName) => {
-  let splittedMovieName = movieName.split(' ');
-  let joinedMovieName = splittedMovieName.join('-');
-  let result = `${movieId}-${joinedMovieName}`;
+  const splittedMovieName = movieName.split(' ');
+  const joinedMovieName = splittedMovieName.join('-');
+  const result = `${movieId}-${joinedMovieName}`;
   return result;
+};
+
+export const getIdFromPath = path => {
+  const result = path.split('-');
+  return Number(result[0]);
 };
--- src/helpers/pathPrettier.test.js
+++ src/helpers/pathPrettier.test.js
@@ -1,9 +1,12 @@
-import { convertToPath } from './pathPrettier';
+import { convertToPath, getIdFromPath } from './pathPrettier';
 
 describe('path prettier helper', () => {
   it('should return prettier path', () => {
     expect(convertToPath('8888', 'Keluarga Ceramah')).toBe(
       '8888-Keluarga-Ceramah'
     );
+  });
+  it('should return an id', () => {
+    expect(getIdFromPath('8888-Keluarga-Ceramah')).toBe(8888);
   });
 });
